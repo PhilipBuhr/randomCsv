9d26804105022ac54173077fffc4e416e379c596
Philip Buhr <philip.buhr@outlook.de>
2020-01-12 16:46:32 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,15 @@ from setuptools import find_packages
 from setuptools import setup
 
 setup(
-    name='randomCsv',
+    name='randomcsv',
     version='0.1.0',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    package_data={'randomcsv': ['src/randomcsv/resources/dictionaries/*.txt']},
+    include_package_data=True,
     url='https://github.com/PhilipBuhr/randomCsv',
+    download_url='https://github.com/PhilipBuhr/randomCsv/archive/0.1.0.tar.gz',
     license='MIT',
     author='Philip Buhr',
     author_email='philip.buhr@buhrwerk.de',
