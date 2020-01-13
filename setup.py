d0e66197920a0a728bdcaac7c4a1fdb30c614f8f
Philip Buhr <philip.buhr@outlook.de>
2020-01-13 20:35:44 +0100

    add MANIFEST.in

--- setup.py
+++ setup.py
@@ -4,13 +4,15 @@ from os.path import splitext, basename
 from setuptools import find_packages
 from setuptools import setup
 
+with open('README.md', 'r') as readme:
+    long_description = readme.read()
+
 setup(
     name='randomcsv',
-    version='0.1.0',
+    version='0.1.3',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
-    package_data={'randomcsv': ['src/randomcsv/resources/dictionaries/*.txt']},
     include_package_data=True,
     url='https://github.com/PhilipBuhr/randomCsv',
     download_url='https://github.com/PhilipBuhr/randomCsv/archive/0.1.0.tar.gz',
@@ -18,5 +20,13 @@ setup(
     author='Philip Buhr',
     author_email='philip.buhr@buhrwerk.de',
     description='For generating specific CSVs for testing data piplines',
-    install_requires=['pandas', 'requests', 'pytest', 'numpy']
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=['pandas', 'requests', 'pytest', 'numpy'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
 )
